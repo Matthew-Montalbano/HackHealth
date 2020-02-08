394500bcfe29c34b46f2f8e93ac8fbdfb05af250
Matthew Montalbano <Matthew.Montalbano@stonyrook.edu>
2020-02-08 12:15:02 -0500

    Updated nutrional constants

---
+++ NutritionalConstants.java
@@ -0,0 +1,13 @@
+protected NutritionalValues {
+	public static final double CARBOHYDRATES = 130;
+	public static final double FAT = null;
+	public static final double FIBER = 38;
+	public static final double PROTEIN = 56;
+	public static final double VITAMIN_A = 900;
+	public static final double VITAMIN_C = 90;
+	public static final double VITAMIN_D = 15;
+	public static final double VITAMIN_E = 15;
+	public static final double VITAMIN_K = 120;
+	public static final double VITAMIN_B6 = 1.3;
+	public static final double VITAMIN_B12 = 2.4;
+}
