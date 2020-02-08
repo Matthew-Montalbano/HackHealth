9b59747ef1c88055a1906da3991cb0f0284fbbae
Matthew Montalbano <Matthew.Montalbano@stonyrook.edu>
2020-02-08 12:37:21 -0500

    updated nutritional constants

--- src/NutritionalConstants.java
+++ src/NutritionalConstants.java
@@ -1,6 +1,8 @@
 public class NutritionalConstants {
 	public static final double CARBOHYDRATES = 130;
-	public static final double FAT = null;
+	public static final double TOTAL_FAT = 60;
+	public static final double SATURATED_FAT = 5;
+	public static final double TRANS_FAT = 0;
 	public static final double FIBER = 38;
 	public static final double PROTEIN = 56;
 	public static final double VITAMIN_A = 900;
